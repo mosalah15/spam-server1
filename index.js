1f93eb97078397526bd8e814a4f7681909bd0db2
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-22 11:48:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -29,14 +29,5 @@ function timerFunc() {
 }
 
 var timer = setTimeout(timerFunc, 1000);
-    }
-});
- client.on("message", function(message) {
-    var args = message.content.split(/ +/g);
-    var command = args.shift()
 
-    if(command == "say") {
-        message.channel.send(args.slice(1, args.length).join(" "))
-    }
-});
 client.login(process.env.BOT_TOKEN);
