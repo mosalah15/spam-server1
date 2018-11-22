c5ece80b897986d808c2151fe0a0229ccbc99a16
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-22 10:31:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("roomID")
+let channel =     client.channels.get("515067564376981506")
 setInterval(function() {
 channel.send(spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam);
 }, 25)
