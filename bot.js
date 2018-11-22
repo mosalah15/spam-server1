d7cfd5a84ca8ec7a8a531df1656dd5188e9bad9f
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-22 10:43:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,33 @@
+var ServerID = "515067466570006528"; //اي دي السيرفر
+var ChannelID = "515067564376981506";// اي دي الروم
+
+
+
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-console.log("Scrpit By Dream");
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
 
 
-client.on("ready", () => {
-let channel =     client.channels.get("515067564376981506")
-setInterval(function() {
-channel.send(spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam);
-}, 25)
-})
- 
- client.on("message", function(message) {
-    var args = message.content.split(/ +/g);
-    var command = args.shift()
 
-    if(command == "say") {
-        message.channel.send(args.slice(1, args.length).join(" "))
-    }
-});
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script'));
+
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
+
+
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7))
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 1000);
+
 client.login(process.env.BOT_TOKEN);
